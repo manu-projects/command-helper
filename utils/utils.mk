0b52009803f6045b04534b3b8448bacee55442f0
Emmanuel <emmanuel.lazarte.012@gmail.com>
2023-03-26 03:25:01 -0300

    Añadido: visualizar observaciones de los comandos

--- utils/utils.mk
+++ utils/utils.mk
@@ -13,16 +13,19 @@ BASH_ALIAS="alias $(BASH_ALIAS_SYMBOL)='make --no-print-directory -C $(CURRENT_D
 	CURRENT_TTY_PATH=$$(pwd) \
 	APP_AUTHOR=$(APP_AUTHOR)"
 
-# alias ?='make --no-print-directory -C /home/jelou/Documents/git/manu-makefiles/command-helper CURRENT_TTY_PATH=$(pwd)'
 BASH_ALIAS_ESCAPE_SLASH=$(subst /,\/,$(BASH_ALIAS))
 
 POPUP_EDIT = sh ./scripts/edit-popup.sh $(TEXT_EDITOR)
 
 APP_INSTALLED=$(shell grep -q "^alias ?='make.*APP_AUTHOR=neverkas" ~/.bash_aliases && echo true)
 
-# es necesario el slash invertido '\' para escalar el caracter '/'
+# es necesario el slash invertido '\' para escapar el caracter '/'
 # porque se utiliza en linux-create-doc en el comando sed
 DATE_NOW_FORMAT=$(shell date +"%d\/%m\/%Y")
 
+# es necesario el slash invertido '\'  para escapar el caracter '#' porque GNU Make interpreta los # como comentarios
 PATTERN_METADATA_DATE=(\#\# ACTUALIZADO:) ([[:digit:]]{1,2}\/[[:digit:]]{1,2}\/[[:digit:]]{4})
 UPDATE_METADATA_DATE=sed -i -E 's/$(PATTERN_METADATA_DATE)/\1 $(DATE_NOW_FORMAT)/i'
+
+# es necesario el slash invertido '\'  para escapar el caracter '#' porque GNU Make interpreta los # como comentarios
+LINE_METADATA_OBSERVACIONES=$(shell grep -n '\#\# OBSERVACIONES:' $(DOC_COMMANDS_LINUX_DIRECTORY)/.template | cut --delimiter=':' --fields=1)
