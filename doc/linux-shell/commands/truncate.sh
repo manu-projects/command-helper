499d6c2c0869f23320e0cc163f4d2c13a990e369
Emmanuel <emmanuel.lazarte.012@gmail.com>
2023-03-26 22:22:25 -0300

    Añadido: doc(linux-shell), ejemplos comando truncate

--- doc/linux-shell/commands/truncate.sh
+++ doc/linux-shell/commands/truncate.sh
@@ -4,7 +4,7 @@
 ## CATEGORIA: operaciones con archivos
 ## DESCRIPCION: recorta el tamaño de un archivo (truncar un archivo)
 ##
-## OBSERVACIONES: -
+## OBSERVACIONES: se puede probar con el comando find, éste busca archivos por su tamaño
 ## ####################################################################
 
 # Limpiar el contenido del archivo, lo deja con 0 Bytes
@@ -14,8 +14,15 @@ truncate -s 0 archivo.txt
 truncate -s 100 archivo.txt
 
 # Aumentar el tamaño del archivo en +100k (es decir 100*1024 Bytes)
-# Nota: las unidades son K,M,G,T,P y son potencias de 1024 Bytes
-truncate -s +100k archivo.txt
+#
+# - las unidades son (K) Kilobytes, (M) Megabytes, (G) Gigabytes, (T) Terabytes, (P) Petabytes
+truncate -s +100K archivo.txt
+
+# Aumentar el tamaño 10 Megabytes
+truncate -s +10M archivo.txt
+
+# Aumentar el tamaño 1 Gigabytes
+truncate -s +1G archivo.txt
 
 # Reducir el tamaño del archivo en -100k
 truncate -s -100k archivo.txt
